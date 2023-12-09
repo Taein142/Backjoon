86a1a73ea360a55d807e4c1633c471ce504247df
Taein142 <xodlsdldy@naver.com>
2023-12-10 03:40:28 +0900

    백준 3단계 중 6번까지 클리어

---
+++ src/Step03/Ex10.java
@@ -0,0 +1,9 @@
+package Step03;
+
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ src/Step03/Ex11.java
@@ -0,0 +1,9 @@
+package Step03;
+
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
