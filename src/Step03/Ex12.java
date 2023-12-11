6e6e9e81fed1c0af0e7317807d95e7eed682be89
Taein142 <xodlsdldy@naver.com>
2023-12-11 12:50:18 +0900

    3챕 완

--- src/Step03/Ex12.java
+++ src/Step03/Ex12.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Ex12 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextInt()) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a + b);
+        }
+        in.close();
     }
 }
