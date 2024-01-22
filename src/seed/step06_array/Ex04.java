9663a0971eca56d577c9be701220d692ff24d8e9
Taein142 <xodlsdldy@naver.com>
2024-01-22 10:38:55 +0900

    배열4

--- src/seed/step06_array/Ex04.java
+++ src/seed/step06_array/Ex04.java
@@ -7,21 +7,26 @@ public class Ex04 {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int M = scanner.nextInt();
-        int[] A = new int[M];
-        int[] B = new int[M];
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                A[j] = scanner.nextInt();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = scanner.nextInt();
             }
         }
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                B[j] = scanner.nextInt();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = scanner.nextInt();
             }
         }
 
         for (int i = 0; i < N; i++) {
-            System.out.println(A[i-1]+B[i-1]);
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
+                if (j == M-1){
+                    System.out.println();
+                }
+            }
         }
     }
 }
