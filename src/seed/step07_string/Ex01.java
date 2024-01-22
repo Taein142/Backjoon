786eae94997f8fc6963b1224fdd8617359615e02
Taein142 <xodlsdldy@naver.com>
2024-01-22 18:19:47 +0900

    문자열 2번까지 완료

---
+++ src/seed/step07_string/Ex01.java
@@ -0,0 +1,7 @@
+package seed.step07_string;
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        int a = System.in.read();
+        System.out.print(a);
+    }
+}
---
+++ src/seed/step07_string/Ex02.java
@@ -0,0 +1,11 @@
+package seed.step07_string;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        System.out.println(word.length());
+    }
+}
