04a0785eb3eece78faf5e2fcfa988565405fff8d
Taein142 <xodlsdldy@naver.com>
2024-01-31 18:24:05 +0900

    오랜만에 백준

---
+++ src/class01/ex04.java
@@ -0,0 +1,24 @@
+package class01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = 0;
+        int maxNum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            num = Integer.parseInt(br.readLine());
+            if (num>maxNum){
+                maxNum = num;
+                count = i;
+            }
+        }
+        System.out.println(maxNum);
+        System.out.println(count);
+    }
+}
