c2234ba5e1b987f64e4e7b0d938210d36900aa7d
Taein142 <xodlsdldy@naver.com>
2024-02-03 01:08:55 +0900

    백준

---
+++ src/check.java
@@ -0,0 +1,9 @@
+//import java.io.BufferedReader;
+//
+//public class check {
+//    public static void main(String[] args) {
+//        BufferedReader br = new BufferedReader()
+//        char ch = br.readline().
+//                charnum = char - 'a' +1
+//    }
+//}
