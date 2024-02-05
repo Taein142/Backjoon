55dea294fa05cc8c9ddde79d1c47b7ea6cbaa705
Taein142 <xodlsdldy@naver.com>
2024-02-05 17:47:50 +0900

    노트 생성

--- src/check.java
+++
@@ -1,9 +0,0 @@
-//import java.io.BufferedReader;
-//
-//public class check {
-//    public static void main(String[] args) {
-//        BufferedReader br = new BufferedReader()
-//        char ch = br.readline().
-//                charnum = char - 'a' +1
-//    }
-//}
