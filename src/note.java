4c4b0761bfe81ea303c5360e981b253407f0d13e
Taein142 <xodlsdldy@naver.com>
2024-02-07 17:09:28 +0900

    노트 수정

--- src/note.java
+++ src/note.java
@@ -1,10 +1,10 @@
 public class note {
     public static void main(String[] args) {
-        //    public static void main(String[] args) {
+
 //        BufferedReader br = new BufferedReader()
 //        char ch = br.readline().
 //                charnum = char - 'a' +1
-//    }
+
     int min = Integer.MAX_VALUE;
     int max = Integer.MIN_VALUE;
 
