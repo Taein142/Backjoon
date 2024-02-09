df4d5d4490fb4dfeefa7e1d26e7d5081c7f32bd3
Taein142 <xodlsdldy@naver.com>
2024-02-09 18:29:51 +0900

    심화 6번 필요 없는 변수 삭제

--- src/step/step06/Ex06.java
+++ src/step/step06/Ex06.java
@@ -9,37 +9,36 @@ public class Ex06 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        int len = str.length();
         int count = 0;
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < str.length(); i++) {
 
             char ch = str.charAt(i);
 
-            if(ch == 'c' && i < len - 1) {
+            if(ch == 'c' && i < str.length() - 1) {
                 if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
                     i++;
                 }
             }
 
-            else if(ch == 'd' && i < len - 1) {
+            else if(ch == 'd' && i < str.length() - 1) {
                 if(str.charAt(i + 1) == '-') {
                     i++;
                 }
-                else if(str.charAt(i + 1) == 'z' && i < len - 2) {
+                else if(str.charAt(i + 1) == 'z' && i < str.length() - 2) {
                     if(str.charAt(i + 2) == '=') {
                         i += 2;
                     }
                 }
             }
 
-            else if((ch == 'l' || ch == 'n') && i < len - 1) {
+            else if((ch == 'l' || ch == 'n') && i < str.length() - 1) {
                 if(str.charAt(i + 1) == 'j') {
                     i++;
                 }
             }
 
-            else if((ch == 's' || ch == 'z') && i < len - 1) {
+            else if((ch == 's' || ch == 'z') && i < str.length() - 1) {
                 if(str.charAt(i + 1) == '=') {
                     i++;
                 }
