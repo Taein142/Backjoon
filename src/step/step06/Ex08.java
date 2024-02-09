2983bd81b6ba895b444f9f8426b1287aa61b1be5
Taein142 <xodlsdldy@naver.com>
2024-02-09 18:42:00 +0900

    심화 7번

---
+++ src/step/step06/Ex08.java
@@ -0,0 +1,10 @@
+package step.step06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
