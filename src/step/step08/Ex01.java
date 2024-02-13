7e2f4cd0f519f3a37f90f645cc8d55b438ec6e28
Taein142 <xodlsdldy@naver.com>
2024-02-13 19:35:52 +0900

    주석 추가

--- src/step/step08/Ex01.java
+++ src/step/step08/Ex01.java
@@ -10,21 +10,24 @@ public class Ex01 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String n = st.nextToken();
-        int b = Integer.parseInt(st.nextToken());
+        String n = st.nextToken(); // 숫자 (근데 문자일수도)
+        int b = Integer.parseInt(st.nextToken()); //진법
 
-        int tmp = 1;
-        int sum = 0;
+        int tmp = 1; // 각 자리의 가중치를 표현하기 위한 변수
+        int sum = 0; // 최종 결과를 저장할 변수
 
+        // 입력받은 숫자를 진법에 따라 10진수로 변환하는 반복문
         for(int i = n.length()-1 ; i >= 0; i--){
-            char ch = n.charAt(i);
+            char ch = n.charAt(i); //입력값 끝자리부터 판별
 
+            // 알파벳인 경우, 해당 알파벳에 대응하는 숫자로 변환하여 계산
             if ('A' <= ch && ch<= 'Z') {
                 sum += (ch - 'A' + 10) * tmp;
             } else {
+                // 숫자인 경우, 그대로 정수로 변환하여 계산
                 sum += (ch - '0') * tmp;
             }
-            tmp *= b;
+            tmp *= b;  // 다음 자리의 가중치를 계산하기 위해 진법만큼 곱셈
         }
 
         System.out.println(sum);
