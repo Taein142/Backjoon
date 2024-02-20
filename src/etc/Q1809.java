edd6dfec7287fdcb1c4fc7615c62bcb040a067ba
Taein142 <xodlsdldy@naver.com>
2024-02-20 17:19:08 +0900

    기타 문제 품

---
+++ src/etc/Q1809.java
@@ -0,0 +1,14 @@
+package etc;
+
+public class Q1809 {
+    public static void main(String[] args) {
+//        "(___)
+//        (o o)____/
+//        @@      \\
+//        \\ ____,/
+//        //   //
+//        ^^   ^^
+//        "puts
+
+    }
+}
