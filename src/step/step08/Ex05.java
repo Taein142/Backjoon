65c0cc2eee4d99ccf29d77f9ee33c23c5800b25e
Taein142 <xodlsdldy@naver.com>
2024-02-24 22:33:09 +0900

    일반수학 5번, 6번

--- src/step/step08/Ex05.java
+++ src/step/step08/Ex05.java
@@ -6,9 +6,21 @@ import java.io.InputStreamReader;
 
 public class Ex05 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[][] arr = null;
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2;
 
+        if (N == 1) {
+            System.out.print(1);
+        } else {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.print(count);
+        }
     }
 }
