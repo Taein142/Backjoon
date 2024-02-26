1842e0b40bd4fa4be44a8f7fbdba3a0a506864aa
Taein142 <xodlsdldy@naver.com>
2024-02-26 20:21:23 +0900

    기타

---
+++ src/etc/Q5539.java
@@ -0,0 +1,18 @@
+package etc;
+
+import java.io.IOException;
+
+public class Q5539 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
